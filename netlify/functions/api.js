655e289b6afdb0128b55aef3f03c83c1732092f8
Iffah Qadeer <iffahqadeer977@gmail.com>
2026-08-12 01:43:20 +0500

    Fix Netlify serverless API

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,4 +1,25 @@
+const express = require("express");
 const serverless = require("serverless-http");
-const app = require("../../backend/server");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const connectDB = require("../../backend/config/db");
+const destinationRoutes = require("../../backend/routes/destinationRoutes");
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// Connect to MongoDB
+connectDB();
+
+// IMPORTANT: no /api here
+app.use("/destinations", destinationRoutes);
+
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to Wanderly API" });
+});
 
 module.exports.handler = serverless(app);
