0a11957cceac9d8c444400772e1e7ff1a31a3a9d
Iffah Qadeer <iffahqadeer977@gmail.com>
2026-08-13 03:26:05 +0500

    Fix destination routes

--- backend/controllers/destinationController.js
+++ backend/controllers/destinationController.js
@@ -1,81 +1,19 @@
-const Destination = require("../models/Destination");
+const express = require("express");
 
-// GET all destinations
-const getDestinations = async (req, res) => {
-  try {
-    const destinations = await Destination.find();
-    res.status(200).json(destinations);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-};
-
-// GET one destination
-const getDestination = async (req, res) => {
-  try {
-    const destination = await Destination.findById(req.params.id);
-
-    if (!destination) {
-      return res.status(404).json({ message: "Destination not found" });
-    }
-
-    res.status(200).json(destination);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-};
-
-// ADD destination
-const addDestination = async (req, res) => {
-  try {
-    const destination = await Destination.create(req.body);
-    res.status(201).json(destination);
-  } catch (error) {
-    res.status(400).json({ message: error.message });
-  }
-};
-
-// UPDATE destination
-const updateDestination = async (req, res) => {
-  try {
-    const destination = await Destination.findByIdAndUpdate(
-      req.params.id,
-      req.body,
-      {
-        new: true,
-        runValidators: true,
-      }
-    );
-
-    if (!destination) {
-      return res.status(404).json({ message: "Destination not found" });
-    }
-
-    res.status(200).json(destination);
-  } catch (error) {
-    res.status(400).json({ message: error.message });
-  }
-};
-
-// DELETE destination
-const deleteDestination = async (req, res) => {
-  try {
-    const destination = await Destination.findByIdAndDelete(req.params.id);
-
-    if (!destination) {
-      return res.status(404).json({ message: "Destination not found" });
-    }
-
-    res.status(200).json({ message: "Destination deleted successfully" });
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-};
-
-module.exports = {
+const {
   getDestinations,
   getDestination,
   addDestination,
   updateDestination,
   deleteDestination,
-};
+} = require("../controllers/destinationController");
+
+const router = express.Router();
+
+router.get("/", getDestinations);
+router.get("/:id", getDestination);
+router.post("/", addDestination);
+router.put("/:id", updateDestination);
+router.delete("/:id", deleteDestination);
+
+module.exports = router;
