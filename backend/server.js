bf60813ca1f871d6d83a589f22df669899bcfd6e
Iffah Qadeer <iffahqadeer977@gmail.com>
2026-08-14 20:57:31 +0500

    Prepare backend for Render

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ module.exports = app;
 if (require.main === module) {
   const PORT = process.env.PORT || 5000;
 
-  app.listen(PORT, () => {
+  app.listen(PORT, "0.0.0.0", () => {
     console.log(`Server running on port ${PORT}`);
   });
 }
